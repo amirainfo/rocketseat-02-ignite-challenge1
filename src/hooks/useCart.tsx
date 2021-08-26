c3c546d677afe366a4288bbf1ca4d5305f78d980
Danilo Parisotto <danilo@amirainfo.com.br>
2021-08-26 07:12:53 -0300

    Finished Challenge - Chapter 2 - carrinho de compras

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,143 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../services/api';
+import { Product } from '../types';
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface UpdateProductAmount {
+  productId: number;
+  amount: number;
+}
+
+interface CartContextData {
+  cart: Product[];
+  addProduct: (productId: number) => Promise<void>;
+  removeProduct: (productId: number) => void;
+  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+}
+
+const CartContext = createContext<CartContextData>({} as CartContextData);
+
+export function CartProvider({ children }: CartProviderProps): JSX.Element {
+  const [cart, setCart] = useState<Product[]>(() => {
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+     }
+
+    return [];
+  });
+
+  const addProduct = async (productId: number) => {
+    try {
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+      // se o produto existe adiciona ele no carrinho
+      if(productExists) {
+        productExists.amount = amount;
+      }else{
+        const product = await api.get(`/products/${productId}`);
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        }
+
+      updatedCart.push(newProduct);
+
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+      }
+
+    } catch {
+      toast.error('Erro na adição do produto');
+      
+    }
+  };
+
+  const removeProduct = (productId: number) => {
+    try {
+      
+      const updatedCart = [...cart];
+
+      const productIndex = updatedCart.findIndex(product => product.id === productId);
+      // o findIndex retorna quando nao encontra -1
+      if (productIndex >=0) {
+        // remove o item do array
+        updatedCart.splice(productIndex, 1);
+        //seta o cart 
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+
+      }else{
+          //Força um erro para sair para o catch
+          throw Error();
+      }
+
+    } catch {
+      toast.error('Erro na remoção do Produto');
+    }
+  };
+
+  const updateProductAmount = async ({
+    productId,
+    amount,
+  }: UpdateProductAmount) => {
+    try {
+      
+      if(amount <=0) {
+        return
+      }
+
+      const stock = await api.get(`/stock/${productId}`)
+
+      const stockAmount = stock.data.updateProductAmount
+
+      if (amount > stockAmount) {
+        toast.error('Erro na alteração de quantidade do produto');
+      }
+
+      const updatedCart = [...cart];
+
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      if(productExists){
+        productExists.amount = amount;
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+      }else{
+        //Força um erro para sair para o catch
+          throw Error();
+      }
+     
+    } catch {
+      toast.error('Erro na alteração de quantidade do produto');
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, updateProductAmount }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart(): CartContextData {
+  const context = useContext(CartContext);
+
+  return context;
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+import { api } from '../../services/api';
+import { useCart } from '../../hooks/useCart';
+import { title } from 'process';
+import { formatPrice } from '../../util/format';
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+interface ProductFormatted extends Product {
+  priceFormatted: string;
+}
+
+interface CartItemsAmount {
+  [key: number]: number;
+}
+
+const Home = (): JSX.Element => {
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
+
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    //criei um novo objeto 
+    const newSumAmount = { ...sumAmount };
+    //acessando a propriedade product dinamicamente e criando e associando com a qtde do prod
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+
+  }, {} as CartItemsAmount)
+
+  useEffect(() => {
+    async function loadProducts() {
+      // usando a tipagem generic no get do AXIOS que passa um array para identificar
+      const response = await api.get<Product[]>('products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price)
+      }))
+
+      setProducts(data);
+
+    }
+
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id: number) {
+    addProduct(id);
+  }
+
+  return (
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+          onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+               {cartItemsAmount[product.id] || 0}
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
+};
+
+export default Home;
